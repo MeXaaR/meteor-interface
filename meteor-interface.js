bf695bfaef82fa53d958fa7e7fb89492543585ed
removedporn <86824510+removedporn@users.noreply.github.com>
2022-03-25 23:24:20 +0800

    Delete meteor-interface.js

--- meteor-interface.js
+++
@@ -1,9 +0,0 @@
-// Write your package code here!
-
-// Variables exported by this module can be imported by other packages and
-// applications. See meteor-interface-tests.js for an example of importing.
-const Interface = 'meteor-interface'
-
-export default Interface
-
-
